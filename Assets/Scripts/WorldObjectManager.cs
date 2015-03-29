09abec0fe3bf205872792640d355f5ed35e54381
Flafla2 <awesomesauceface@aol.com>
2015-03-28 23:53:18 -0400

    Shader Finished

---
+++ Assets/Scripts/WorldObjectManager.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class WorldObjectManager : MonoBehaviour {
+
+    public Transform worldInsideTransform;
+    public Transform worldOutsideTransform;
+
+    public Transform changerTransform;
+    public Transform playerTransform;
+
+    [SerializeField]
+    private TexturizerState _State;
+    public TexturizerState State
+    {
+        get { return _State; }
+        set { _State = value; UpdateShaderParamsAll(); }
+    }
+
+    [SerializeField]
+    private string _ShaderName;
+    public string ShaderName
+    {
+        get { return _ShaderName; }
+        set { _ShaderName = value; UpdateShaderParamsAll(); }
+    }
+
+    [SerializeField]
+    private Color _BorderColor;
+    public Color BorderColor
+    {
+        get { return _BorderColor; }
+        set { _BorderColor = value; UpdateShaderParamsAll(); }
+    }
+
+    [SerializeField]
+    private float _BorderWidth;
+    public float BorderWidth
+    {
+        get { return _BorderWidth; }
+        set { _BorderWidth = value; UpdateShaderParamsAll(); }
+    }
+
+    [SerializeField]
+    private Dimension _PrimaryColor; // True = blue, False = orange
+    public Dimension PrimaryColor
+    {
+        get { return _PrimaryColor; }
+        set { _PrimaryColor = value; UpdateShaderParamsAll(); }
+    }
+
+    [SerializeField]
+    private float _LightRadius; // True = blue, False = orange
+    public float LightRadius
+    {
+        get { return _LightRadius; }
+        set { _LightRadius = value; UpdateShaderParamsAll(); }
+    }
+
+    private Dimension PlayerDimension;
+
+
+	// Use this for initialization
+	void Start () {
+        UpdateShaderParamsAll();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateShaderParamsAll();
+	}
+
+    private void UpdateShaderParamsAll()
+    {
+        UpdateShaderParamsRecursive(worldInsideTransform, Dimension.BLUE);
+        UpdateShaderParamsRecursive(worldOutsideTransform, Dimension.ORANGE);
+    }
+
+    private void UpdateShaderParamsRecursive(Transform t, Dimension dim)
+    {
+        foreach (Transform c in t)
+        {
+            UpdateShaderParams(c,dim);
+            UpdateShaderParamsRecursive(c,dim);
+        }
+    }
+
+    private void UpdateShaderParams(Transform obj, Dimension dim)
+    {
+        // Shader Code
+#if UNITY_EDITOR
+        Material mat = obj.GetComponent<Renderer>().sharedMaterial;
+#else
+        Material mat = obj.GetComponent<Renderer>().material;
+#endif
+        if (!mat.shader.name.Equals(ShaderName))
+            return;
+
+        mat.SetVector("_LightPos", changerTransform.position);
+        mat.SetFloat("_TexturizerState", State == TexturizerState.RADIAL ? 1 : 0);
+        bool Invert = dim != _PrimaryColor;
+        mat.SetFloat("_Inverted", Invert ? 1 : 0);
+        mat.SetFloat("_LightRad", _LightRadius);
+        mat.SetColor("_BorderColor", _BorderColor);
+        mat.SetFloat("_BorderWidth", _BorderWidth);
+
+        // Collision Code
+#if !UNITY_EDITOR
+        BoxCollider box = obj.GetComponent<BoxCollider>();
+        if (box != null)
+            box.enabled = PlayerDimension == dim;
+#endif
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(changerTransform.position,_LightRadius);
+    }
+
+    [System.Serializable]
+    public enum TexturizerState
+    {
+        RADIAL, FLAT
+    }
+
+    [System.Serializable]
+    public enum Dimension
+    {
+        BLUE, ORANGE
+    }
+}
---
+++ Assets/Shader/WorldObject.shader
@@ -0,0 +1,81 @@
+﻿Shader "BLINK/World Object" {
+	Properties {
+		_Color ("Color (Primary/Far)", Color) = (1,1,1,1)
+		_Albedo ("Albedo (Primary/Far)", 2D) = "white" {}
+
+		_Smoothness ("Smoothness", Range(0,1)) = 0.5
+		_Metallic ("Metallic", Range(0,1)) = 0.0
+
+		[HideInInspector] _BorderColor ("Border Color", Color) = (1,1,1,1)
+		[HideInInspector] _BorderWidth ("Border Width", Float) = 0.1
+		[HideInInspector] _LightPos ("Texturer Position", Vector) = (0,0,0,0)
+		[HideInInspector] _LightRad ("Texturer Radius", Float) = 1.0
+		[HideInInspector] _Cylindrical ("Cylindrical (Ignore Y axis)", Float) = 0
+		
+		[HideInInspector] _TexturizerState ("Texturizer State", Float) = 0 // 0 = full, 1 = radial -- used to support both types of switching
+		[HideInInspector] _Inverted ("Invert State", Float) = 0 // 0 = normal, 1 = inverted -- used to invert the world via scripting
+	}
+	SubShader {
+		Tags { "RenderType"="TransparentCutout" "Queue"="AlphaTest" }
+		
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma surface surf Standard fullforwardshadows
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		sampler2D _Albedo;
+		half4 _Color;
+
+		float _Smoothness;
+		float _Metallic;
+		
+		half4 _BorderColor;
+		float _BorderWidth;
+
+		float4 _LightPos;
+		float _LightRad;
+
+		half _Cylindrical;
+		half _TexturizerState;
+		half _Inverted;
+
+		struct Input {
+			float2 uv_AlbedoPrimary;
+			float3 worldPos;
+		};
+
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			float3 d = (float3)_LightPos-IN.worldPos;
+			// Using Distance Squared eliminates the need for a costly square root calculation
+			float distSq = d.x*d.x + d.z*d.z + (1.0f-_Cylindrical)*d.y*d.y;
+			float farDistSq = _LightRad+_BorderWidth/2; farDistSq *= farDistSq;
+			float nearDistSq = _LightRad-_BorderWidth/2; nearDistSq *= nearDistSq;
+             
+			// Alpha (gradient between inner and outer texture) is calculated using the distance
+			// saturate() is used to limit the alpha to the range 0 - 1
+			float alpha = saturate((distSq-nearDistSq)/(farDistSq-nearDistSq));
+
+			// Border alpha is the alpha of the border (0-1) at a point.  This is cast to an integer so there is a hard border.
+			// Remove (int) to get a soft gradient border.
+			float border_alpha = (int)abs(alpha*2-1) * _TexturizerState;
+
+			if(!_TexturizerState) {alpha = 1;border_alpha = 1;} // Ugly if statements in a shader
+			if(_Inverted) alpha = 1-alpha;
+			
+			if(alpha == 0) discard;
+
+			half4 c = tex2D (_Albedo, IN.uv_AlbedoPrimary) * _Color;
+
+			o.Albedo = lerp(fixed3(0,0,0),c,border_alpha);
+			//o.Alpha = lerp(c_n.a,c_f.a,alpha);
+			o.Smoothness = _Smoothness;
+			o.Metallic = _Metallic;
+             
+			o.Emission = lerp((fixed3)_BorderColor,fixed3(0,0,0),border_alpha);
+		}
+		ENDCG
+	} 
+	FallBack "Diffuse"
+}
